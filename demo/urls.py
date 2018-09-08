1fffdfee342ede7dd20e51bf6495d24bbbd5bb89
srikanthpragada <srikanthpragada@gmail.com>
2018-09-08 18:00:27 +0530

    08-SEP

--- demo/urls.py
+++ demo/urls.py
@@ -5,5 +5,8 @@ from . import views
 
 urlpatterns = [
     path('product/', views.show_product),
+    path('products/', views.show_products_list),
+    path('netprice/', views.net_price_calculator),
+    path('netprice2/', views.net_price),
 
 ]
--- demo/views.py
+++ demo/views.py
@@ -5,6 +5,38 @@ from .models import Product
 
 # Create your views here.
 
+def  net_price_calculator(request):
+    netamount = 0
+    amount = ""
+    qty = ""
+    if 'amount' in request.GET:
+        # calculate net amount
+        amount = int(request.GET['amount'])
+        qty = int(request.GET['qty'])
+        netamount = amount * qty
+        if qty > 5:
+            netamount = netamount * 0.90
+
+    return render(request,'net_price_calculator.html',
+                   {'netamount': netamount, "amount" : amount, "qty": qty})
+
+
+def  net_price(request):
+    print("Request type : ", request.method)
+    if request.method == "POST":
+        # calculate net amount
+        amount = int(request.POST['amount'])
+        qty = int(request.POST['qty'])
+        netamount = amount * qty
+        if qty > 5:
+            netamount = netamount * 0.90
+        return render(request,'net_price.html',
+                   {'netamount': netamount, "amount" : amount, "qty": qty})
+    else:
+        return render(request, 'net_price.html')
+
+
+
 def show_product(request):
     p = Product("iPhone X", 80000)  # Model
     return render(request, 'product_info.html', {"product": p})
@@ -14,4 +46,8 @@ def show_products_list(request):
     products = [Product("iPhone X", 80000),
                 Product("Google Pixel XL", 60000),
                 Product("Samsung S9 Plus", 70000)]
-    return render(request, 'products_list.html', {"products": products})
+
+    avg = sum([p.price for p in products]) / len(products)
+
+    return render(request, 'products_list.html',
+                   {"products": products , "average" : avg})
