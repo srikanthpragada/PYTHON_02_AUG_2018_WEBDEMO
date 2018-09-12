608f0f6e14f46e3f9dc3302e7e149df4702b16bc
srikanthpragada <srikanthpragada@gmail.com>
2018-09-12 18:00:29 +0530

    12-SEP

--- demo/dept_views.py
+++ demo/dept_views.py
@@ -1,9 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import AddDeptForm
 import sqlite3
 
 
 def add_dept(request):
-    return render(request, 'add_dept.html')
+    message = ""
+    if request.method == "POST":
+        f = AddDeptForm(request.POST)
+        if f.is_valid():
+            name = f.cleaned_data["name"]
+            location = f.cleaned_data["location"]
+
+            # insert into DEPARTMENTS table
+            try:
+                con = sqlite3.connect(r"e:\classroom\python\aug2\hr.db")
+                cur = con.cursor()
+                cur.execute("insert into departments(deptname,location) values (?,?)",
+                            (name, location))
+                con.commit()
+                return redirect("/demo/list_depts")
+            except Exception as ex:
+                print("Database error ->", ex)
+                message = "Could not add department!"
+            finally:
+                con.close()
+    else:
+        f = AddDeptForm()
+
+    return render(request, 'add_dept.html', {"form": f, "message": message})
 
 
 def list_depts(request):
--- demo/forms.py
+++ demo/forms.py
@@ -4,3 +4,13 @@ from django import forms
 class NetPriceForm(forms.Form):
     amount = forms.IntegerField(label="Selling Price")
     qty = forms.IntegerField(label="Quantity Required", min_value=1, max_value=10)
+
+
+class AddDeptForm(forms.Form):
+    name = forms.CharField(label="Department Name", max_length=30)
+    location = forms.ChoiceField(label="Department Location",
+                                 # (value,text)
+                                 choices=[("Mumbai","Mumbai"),
+                                          ("Delhi","Delhi"),
+                                          ("Chennai", "Chennai"),])
+
