de0294748ffe24f4d83238f38d5b7eda04153742
srikanthpragada <srikanthpragada@gmail.com>
2018-09-20 17:59:52 +0530

    20-SEP

--- demo/models.py
+++ demo/models.py
@@ -7,3 +7,14 @@ class Product:
     def __init__(self, name, price):
         self.name = name
         self.price = price
+
+
+class Book (models.Model):
+    bookid = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=50)
+    price = models.IntegerField()
+    author = models.CharField(max_length=50, null=True)
+
+    def __str__(self):
+        """Return a string representation of the model."""
+        return self.title
